7a466ff8103633fe29412b401546339ad954048d
yatofrmdaowic-netizen <yatofrmdaowic@gmail.com>
2026-03-22 17:34:01 +0530

    Add Footer component with copyright information

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ padding: "20px", textAlign: "center", background: "#1f1f1f", marginTop: "50px" }}>
+      <p>© 2026 GhostBlade Hub | All rights reserved</p>
+    </footer>
+  );
+}
